4e0eca4da1c9cddcce260d0820c4598a2c8b7d18
İbrahim <iuyar99@gmail.com>
2023-02-17 23:23:27 +0300

    Add new extensions for get release year and changing selected category background

--- app/src/main/java/com/ri/movieto/util/Util.kt
+++ app/src/main/java/com/ri/movieto/util/Util.kt
@@ -1,6 +1,9 @@
 package com.ri.movieto.util
 
+import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -19,5 +22,30 @@ fun ImageView.downloadFromUrl(url: String?) {
 
 @BindingAdapter("android:downloadUrl")
 fun downloadImage(view: ImageView, url: String?) {
-    view.downloadFromUrl(url)
+    val posterPath = "https://www.themoviedb.org/t/p/w533_and_h300_bestv2$url"
+    view.downloadFromUrl(posterPath)
+}
+
+fun TextView.getYearFromDate(releaseDate: String) {
+    val releaseDateArray = releaseDate.split("-")
+    val releaseYear = releaseDateArray[0]
+    this.text = releaseYear
+}
+
+@BindingAdapter("android:setTextFromDate")
+fun getYear(view: TextView, releaseDate: String) {
+    view.getYearFromDate(releaseDate)
+}
+
+fun TextView.changeBgBySelectedCategory(id: Int) {
+    if (id == 0) {
+        this.setBackgroundColor(ContextCompat.getColor(context, R.color.pink_panther))
+    }else{
+        this.setBackgroundColor(ContextCompat.getColor(context, R.color.midnight))
+    }
+}
+
+@BindingAdapter("android:categoryId")
+fun getCategoryId(view: TextView, id: Int) {
+    view.changeBgBySelectedCategory(id)
 }
