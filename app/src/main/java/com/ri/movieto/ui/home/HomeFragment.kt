5f4e42aadc69dbeec8326d48ff3b9665536a4e78
İbrahim <iuyar99@gmail.com>
2023-02-17 23:27:50 +0300

    Get data from API and display it on screen

--- app/src/main/java/com/ri/movieto/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ri/movieto/ui/home/HomeFragment.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ProgressBar
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.ri.movieto.adapters.CategoryAdapter
+import com.ri.movieto.adapters.TrendingMoviesAdapter
 import com.ri.movieto.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var homeViewModel: HomeViewModel
+    private val trendingMoviesAdapter = TrendingMoviesAdapter(arrayListOf())
+    private val categoryAdapter = CategoryAdapter(arrayListOf())
+    private lateinit var rvTrendingMovies: RecyclerView
+    private lateinit var rvCategory: RecyclerView
+    private lateinit var contentLayout: NestedScrollView
+    private lateinit var dataLoading: ProgressBar
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,17 +34,46 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        homeViewModel =
+            ViewModelProvider(this)[HomeViewModel::class.java]
+        homeViewModel.loadData()
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        rvTrendingMovies = binding.rvTrendingMovies
+        rvTrendingMovies.adapter = trendingMoviesAdapter
+        rvCategory = binding.rvCategory
+        rvCategory.adapter = categoryAdapter
 
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
-        return root
+        contentLayout = binding.contentLayout
+        dataLoading = binding.dataLoading
+
+        observeLiveData()
+
+        return binding.root
+    }
+
+    private fun observeLiveData() {
+        homeViewModel.movies.observe(viewLifecycleOwner, Observer { movies ->
+            movies?.let {
+                contentLayout.visibility = View.VISIBLE
+                trendingMoviesAdapter.updateMovieList(it)
+            }
+        })
+        homeViewModel.genres.observe(viewLifecycleOwner, Observer { genres ->
+            genres?.let{
+                categoryAdapter.updateCategoryList(it)
+            }
+        })
+        homeViewModel.movieLoading.observe(viewLifecycleOwner, Observer { loading ->
+            loading?.let {
+                if(it){
+                    contentLayout.visibility = View.GONE
+                    dataLoading.visibility = View.VISIBLE
+                }else{
+                    dataLoading.visibility = View.GONE
+                }
+            }
+        })
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/ri/movieto/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/ri/movieto/ui/home/HomeViewModel.kt
@@ -1,13 +1,83 @@
 package com.ri.movieto.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ri.movieto.data.model.GenreResponse
+import com.ri.movieto.data.model.MovieResponse
+import com.ri.movieto.data.service.MediaAPIService
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.observers.DisposableSingleObserver
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _movies = MutableLiveData<List<MovieResponse.Movie>?>()
+    private val _movieError = MutableLiveData<Boolean>()
+    private val _movieLoading = MutableLiveData<Boolean>()
+
+    private val _genres = MutableLiveData<List<GenreResponse.Genre>?>().apply {
+        value = listOf(GenreResponse.Genre(0, "All"))
     }
-    val text: LiveData<String> = _text
+
+    val movies: LiveData<List<MovieResponse.Movie>?> get() = _movies
+    val movieError: LiveData<Boolean> get() = _movieError
+    val movieLoading: LiveData<Boolean> get() = _movieLoading
+
+    val genres: LiveData<List<GenreResponse.Genre>?> get() = _genres
+
+    private val disposable = CompositeDisposable()
+    private val mediaAPIService = MediaAPIService()
+
+    fun refreshData() {
+        loadMovies()
+    }
+
+    fun loadData() {
+        loadMovies()
+        loadGenres()
+    }
+
+    private fun loadMovies() {
+        _movieLoading.value = true
+        disposable.add(
+            mediaAPIService.getTrendingMovies()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { response ->
+                        _movies.value = response.results
+                        _movieError.value = false
+                        _movieLoading.value = false
+                    },
+                    { error ->
+                        _movieError.value = true
+                        _movieLoading.value = false
+                        error.printStackTrace()
+                    }
+                )
+        )
+    }
+
+    private fun loadGenres() {
+        disposable.add(
+            mediaAPIService.getMovieGenres()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { response ->
+                        _genres.value = _genres.value?.plus(response.genres)
+                        Log.e("genres", response.genres.toString())
+                    },
+                    { error ->
+                        error.printStackTrace()
+                    }
+                )
+        )
+    }
 }
