4323ff0caf79371b8351da0cfb9c79c52d662609
İbrahim <iuyar99@gmail.com>
2023-02-17 23:24:39 +0300

    Create adapters for category and trending movies

---
+++ app/src/main/java/com/ri/movieto/adapters/CategoryAdapter.kt
@@ -0,0 +1,39 @@
+package com.ri.movieto.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.ri.movieto.R
+import com.ri.movieto.data.model.GenreResponse
+import com.ri.movieto.databinding.ItemCategoryBinding
+
+class CategoryAdapter(private val categoryList: ArrayList<GenreResponse.Genre>) :
+    RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
+
+    class CategoryViewHolder(val view: ItemCategoryBinding) : RecyclerView.ViewHolder(view.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = DataBindingUtil.inflate<ItemCategoryBinding>(
+            inflater, R.layout.item_category, parent, false
+        )
+        return CategoryViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return categoryList.size
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        holder.view.category = categoryList[position]
+    }
+
+    fun updateCategoryList(newList: List<GenreResponse.Genre>){
+        categoryList.clear()
+        categoryList.addAll(newList)
+        notifyDataSetChanged()
+    }
+}
